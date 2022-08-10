072ccd2aa63c7ba39e49b23d1e4c5b5413e6517b
nijama <nijama.dev@gmail.com>
2022-08-10 08:52:15 -0500

    setup.py

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Universidad-GUIFuentes
+
+## Instalar requerimientos:
+
+        python setup.py build
+
+## Iniciar programa:
+
+        python universidad/__init__.py
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='universidad',
+      version='0.1',
+      description='Proyecto Complejidad y Optimización, Escuela de Ingeniería de Sistemas y Computación',
+      url='https://github.com/nijamaDev/Universidad-GUIFuentes',
+      author='nijama, Git-Fanfo, SJMC29, learningalone',
+      license='MIT',
+      packages=['universidad'],
+      install_requires=[
+          'PySimpleGUIQt',
+      ],
+      zip_safe=False)
