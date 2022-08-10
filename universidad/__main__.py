46dc37a356514bdb3a6ce4703f8677ac34469f76
nijama <nijama.dev@gmail.com>
2022-08-10 09:52:30 -0500

    cleaner code

--- universidad/__main__.py
+++ universidad/__main__.py
@@ -1,72 +1,17 @@
-import PySimpleGUIQt as sg
-import os.path
+""" 
+  Proyecto
+  Complejidad y Optimización
+  Escuela de Ingeniería de Sistemas y Computación
+  
+Autores:
+    Alejandro Caicedo - 1827260 - learningalone
+    Juan José Dorado  - 1827439 - Git-Fanfo
+    Nicolás Jaramillo - 1840558 - nijamaDev
+    Sara Maradiago    - 1741648 - SJMC29
 
-# First the window layout in 2 columns
-file_list_column = [
-    [
-        sg.Text("Image Folder"),
-        sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
-        sg.FolderBrowse(),
-    ],
-    [
-        sg.Listbox(
-            values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"
-        )
-    ],
-]
+"""
 
-image_viewer_column = [
-    [sg.Text("Choose an image from list on left:")],
-    [sg.Text(size=(40, 1), key="-TOUT-")],
-    [sg.Image(key="-IMAGE-")],
-]
-
-# ----- Full layout -----
-
-layout = [
-    [
-        sg.Column(file_list_column, key="-TEST-"),
-        sg.VSeperator(),
-        sg.Column(image_viewer_column),
-    ]
-]
-layoutest = [
-    [
-        sg.Column(image_viewer_column)
-    ]
-]
+from universidad import universidadGUI as gui
 
 if __name__ == "__main__":
-    window = sg.Window("Image Viewer", layout)
-
-    while True:
-        event, values = window.read()
-        if event == "Exit" or event == sg.WIN_CLOSED:
-            break
-        # Folder name was filled in, make a list of files in the folder
-        if event == "-FOLDER-":
-            folder = values["-FOLDER-"]
-            try:
-                # Get list of files in folder
-                file_list = os.listdir(folder)
-            except:
-                file_list = []
-            fnames = [
-                f
-                for f in file_list
-                if os.path.isfile(os.path.join(folder, f))
-                and f.lower().endswith((".png", ".gif"))
-            ]
-            window["-FILE LIST-"].update(fnames)
-        elif event == "-FILE LIST-":  # A file was chosen from the listbox
-            try:
-                filename = os.path.join(
-                    values["-FOLDER-"], values["-FILE LIST-"][0]
-                )
-                # window["-TEST-"].update(sg.VSeperator())
-                window["-TOUT-"].update(filename)
-                window["-IMAGE-"].update(filename=filename)
-            except:
-                pass
-
-    window.close()
+    gui.Main()
---
+++ universidad/universidadGUI.py
@@ -0,0 +1,73 @@
+import PySimpleGUIQt as sg
+import os.path
+
+# First the window layout in 2 columns
+file_list_column = [
+    [
+        sg.Text("Image Folder"),
+        sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
+        sg.FolderBrowse(),
+    ],
+    [
+        sg.Listbox(
+            values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"
+        )
+    ],
+]
+
+image_viewer_column = [
+    [sg.Text("Choose an image from list on left:")],
+    [sg.Text(size=(40, 1), key="-TOUT-")],
+    [sg.Image(key="-IMAGE-")],
+]
+
+# ----- Full layout -----
+
+layout = [
+    [
+        sg.Column(file_list_column, key="-TEST-"),
+        sg.VSeperator(),
+        sg.Column(image_viewer_column),
+    ]
+]
+layoutest = [
+    [
+        sg.Column(image_viewer_column)
+    ]
+]
+
+
+def Main():
+    window = sg.Window("Image Viewer", layout)
+
+    while True:
+        event, values = window.read()
+        if event == "Exit" or event == sg.WIN_CLOSED:
+            break
+        # Folder name was filled in, make a list of files in the folder
+        if event == "-FOLDER-":
+            folder = values["-FOLDER-"]
+            try:
+                # Get list of files in folder
+                file_list = os.listdir(folder)
+            except:
+                file_list = []
+            fnames = [
+                f
+                for f in file_list
+                if os.path.isfile(os.path.join(folder, f))
+                and f.lower().endswith((".png", ".gif"))
+            ]
+            window["-FILE LIST-"].update(fnames)
+        elif event == "-FILE LIST-":  # A file was chosen from the listbox
+            try:
+                filename = os.path.join(
+                    values["-FOLDER-"], values["-FILE LIST-"][0]
+                )
+                # window["-TEST-"].update(sg.VSeperator())
+                window["-TOUT-"].update(filename)
+                window["-IMAGE-"].update(filename=filename)
+            except:
+                pass
+
+    window.close()
