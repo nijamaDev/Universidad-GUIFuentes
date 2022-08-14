e4b7e98fdc10e7bc89919c7e735cd34ecaa97f1a
AlejoCx2 <alejandro.caicedo.caicedo@correounivalle.edu.co>
2022-08-14 17:37:36 -0500

    Comentarios en el modelo

--- UniversidadInterfazFuentes/__main__.py
+++ UniversidadInterfazFuentes/__main__.py
@@ -4,7 +4,7 @@
       Escuela de Ingeniería de Sistemas y Computación
       
     Autores:
-        Alejandro Caicedo - 1827260 - learningalone
+        Alejandro Caicedo - 1827260 - AlejoCx2
         Juan José Dorado  - 1827439 - Git-Fanfo
         Nicolás Jaramillo - 1840558 - nijamaDev
         Sara Maradiago    - 1741648 - SJMC29
