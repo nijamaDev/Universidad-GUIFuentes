ddf7f17a13ae101f53dcb13146f83c1ea69110a5
nijama <nijama.dev@gmail.com>
2022-08-14 23:53:37 -0500

    final

--- UniversidadInterfazFuentes/universidad/universidadGUI.py
+++ UniversidadInterfazFuentes/universidad/universidadGUI.py
@@ -1,3 +1,16 @@
+""" 
+      Proyecto
+      Complejidad y Optimización
+      Escuela de Ingeniería de Sistemas y Computación
+      
+    Autores:
+        Alejandro Caicedo - 1827260 - AlejoCx2
+        Juan José Dorado  - 1827439 - Git-Fanfo
+        Nicolás Jaramillo - 1840558 - nijamaDev
+        Sara Maradiago    - 1741648 - SJMC29
+
+"""
+
 import os.path
 import PySimpleGUIQt as sg
 from minizinc import Instance, Model, Solver
@@ -80,7 +93,10 @@ def makeLayout():
             sg.Stretch()
         ],
         [
-            sg.Multiline(size=(40, m+5), key='textbox')
+            sg.Multiline(
+                key='textbox',
+                size=(40, m+5),
+                background_color="#dee")
         ]
     ]
     layout = [
