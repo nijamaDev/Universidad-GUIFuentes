61355a8665c0550edf032438a435085d7e2adc03
nijama <nijama.dev@gmail.com>
2022-08-15 01:43:46 -0500

    1.0

--- UniversidadInterfazFuentes/README.md
+++ UniversidadInterfazFuentes/README.md
@@ -1,4 +1,22 @@
-# Universidad-GUIFuentes
+# Universidad-InterfazFuentes
+
+Proyecto Complejidad y Optimización
+
+Escuela de Ingeniería de Sistemas y Computación
+
+Autores:
+
+Alejandro Caicedo - 1827260 - AlejoCx2
+
+Juan José Dorado - 1827439 - Git-Fanfo
+
+Nicolás Jaramillo - 1840558 - nijamaDev
+
+Sara Maradiago - 1741648 - SJMC29
+
+## Nota
+
+Todas las pruebas de gui fueron realizadas en linux
 
 ## Instalar requerimientos:
 
--- UniversidadInterfazFuentes/setup.py
+++ UniversidadInterfazFuentes/setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='universidad',
-      version='0.1',
+      version='1.0',
       description='Proyecto Complejidad y Optimización, Escuela de Ingeniería de Sistemas y Computación',
       url='https://github.com/nijamaDev/Universidad-GUIFuentes',
-      author='nijama, Git-Fanfo, SJMC29, learningalone',
+      author='nijama, Git-Fanfo, SJMC29, AlejoCx2',
       license='MIT',
       packages=find_packages(),
       package_data={p: ["*"] for p in find_packages()},
